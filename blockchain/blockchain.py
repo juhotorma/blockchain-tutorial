0902bd62c9ae3da1ef04d384311967af1375fbc0
juho <juho.torma@posteo.org>
2020-12-26 21:08:49 +0200

    Finished course

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -6,23 +6,43 @@ import binascii
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA
+from uuid import uuid4
+import json
+import hashlib
+import requests
+from urllib.parse import urlparse
 
 MINING_SENDER = "The Blockchain"
+MINING_REWARD = 1
+MINING_DIFFICULTY = 2
 
 class Blockchain :
 
     def __init__(self):
+        self.node_id = str(uuid4()).replace('-','')
         self.transactions = []
         self.chain = []
+        self.nodes = set()
         # Create the genesis block
         self.create_block(0, "00")
+
+    def register_node(self, node_url) :
+        parsed_url = urlparse(node_url)
+
+        if parsed_url.netloc :
+            self.nodes.add(parsed_url.netloc)
+        elif parsed_url.path :
+            self.nodes.add(parsed_url.path)
+        else :
+            raise ValueError('Invalid URL')
+
 
     def create_block(self, nonce, previous_hash) :
         """
         Add a block of transactions to the blockchain
         """
         block = {
-            'block_number' : len(self.chain) + 1, 
+            'block_number' : len(self.chain) + 1,
             'timestamp' : time(),
             'transactions' : self.transactions,
             'nonce' : nonce,
@@ -33,6 +53,8 @@ class Blockchain :
         self.transactions = []
 
         self.chain.append(block)
+
+        return block
 
     def verify_transaction_signature(self, sender_public_key, signature,
                                      transaction) :
@@ -45,6 +67,81 @@ class Blockchain :
             return True
         except ValueError :
             return False
+
+    @staticmethod
+    def valid_proof(transactions, last_hash, nonce, difficulty = MINING_DIFFICULTY) :
+        guess = (str(transactions) + str(last_hash) + str(nonce)).encode('utf8')
+
+        h = hashlib.new('sha256')
+        h.update(guess)
+
+        guess_hash = h.hexdigest()
+        return guess_hash [:difficulty] == '0' * difficulty
+
+    def proof_of_work(self) :
+        last_block = self.chain[-1]
+        last_hash = self.hash(last_block)
+
+        nonce = 0
+        while self.valid_proof(self.transactions, last_hash, nonce) is False :
+            nonce += 1
+
+        return nonce
+
+    @staticmethod
+    def hash(block):
+        # We must ensure that the dictionary is ordered
+        # Otherwise we'll get inconsistent hashes
+        block_string = json.dumps(block, sort_keys = True).encode('utf8')
+        h = hashlib.new('sha256')
+        h.update(block_string)
+
+        return h.hexdigest()
+
+    def resolve_conflicts(self) :
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+        for node in neighbours :
+            response = requests.get('http://' + node + '/chain')
+
+            if response.status_code == 200 :
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+
+        if new_chain is not None :
+            self.chain = new_chain
+            return True
+
+        return False
+
+    def valid_chain(self, chain) :
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain) :
+            block = chain[current_index]
+            if block['previous_hash'] != self.hash(last_block) :
+                return False
+
+            transactions = block['transactions'][:-1]
+            transaction_elements = ['sender_public_key', 'recipient_public_key', 'amount']
+            transactions = [OrderedDict((k, transaction[k]) for k in transaction_elements) for transaction in transactions]
+
+            if not self.valid_proof(transactions, block['previous_hash'], block['nonce'], MINING_DIFFICULTY) :
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
 
     def submit_transaction(self, sender_public_key, recipient_public_key,
                            signature, amount) :
@@ -80,10 +177,50 @@ CORS(app)
 def index() :
     return render_template('./index.html')
 
+@app.route('/configure')
+def configure() :
+    return render_template('./configure.html')
+
 @app.route('/transactions/get', methods=['GET'])
 def get_transactions() :
     transactions = blockchain.transactions
     response = {'transactions' : transactions}
+    return jsonify(response), 200
+
+@app.route('/mine', methods=['GET'])
+def mine() :
+    # We run the proof of work algorithm
+    nonce = blockchain.proof_of_work()
+
+    # Reward miner
+    blockchain.submit_transaction(
+                sender_public_key = MINING_SENDER, 
+                recipient_public_key = blockchain.node_id, 
+                signature = '',
+                amount = MINING_REWARD
+            )
+
+    last_block = blockchain.chain[-1]
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.create_block(nonce, previous_hash)
+
+    response = {
+            'message' : "New block created",
+            'block_number' : block['block_number'],
+            'transactions' : block['transactions'],
+            'nonce' : block['nonce'],
+            'previous_hash' : block['previous_hash'],
+            }
+
+    return jsonify(response), 200
+
+@app.route('/chain', methods=['GET'])
+def get_chain() :
+    response = {
+                'chain' : blockchain.chain,
+                'length' : len(blockchain.chain)
+            }
+
     return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
@@ -109,6 +246,51 @@ def new_transaction() :
         response = {'message' : 'Transaction will be added to the Block ' +
                     str(transaction_results)}
         return jsonify(response), 201
+
+@app.route('/nodes/get', methods=['GET'])
+def get_nodes() :
+    nodes = list(blockchain.nodes)
+
+    response = {
+            'nodes' : nodes
+            }
+
+    return jsonify(response), 200
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_node() :
+    values = request.form
+    # 127.0.0.1:5002,127.0.0.1:5003,127.0.0.1:5004
+    nodes = values.get('nodes').replace(' ', '').split(',')
+
+    if nodes is None :
+        return 'Error : Please supply a valid list of nodes', 400
+
+    for node in nodes :
+        blockchain.register_node(node)
+
+    response = {
+            'message' : 'Nodes have been added',
+            'total_nodes' : [node for node in blockchain.nodes]
+            }
+
+    return jsonify(response), 200
 
 if __name__ == '__main__' :
     from argparse import ArgumentParser
